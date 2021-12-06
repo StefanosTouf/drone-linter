def782800777f02a7ed05c254f28acb853912cfa
StefanosTouf <stefetoufe@gmail.com>
2021-12-06 17:46:07 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to drone-clojure-config
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/drone_clojure_config/core_test.clj
@@ -0,0 +1,7 @@
+(ns drone-clojure-config.core-test
+  (:require [clojure.test :refer :all]
+            [drone-clojure-config.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
